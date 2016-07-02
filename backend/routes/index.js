d6ff77a4c6cd5eb32a4147c454a877731c7508a0
Kenn Chen <kenn.chen.work@gmail.com>
2016-07-02 21:15:49 +0900

    improvement

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+/*	request("http://www.baidu.com/", function(err, response, body) {
+    // JSON body
+	    if(err) { console.log(err); callback(true); return; }
+	    //obj = JSON.parse(body);
+	    //callback(false, obj);
+	    console.log(body);
+	});*/
   res.render('index', { title: 'Express' });
 });
 
